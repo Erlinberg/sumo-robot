71904e6bea88ae6b96cb65d427d437c8abc3eb30
Erlinberg <42579042+Erlinberg@users.noreply.github.com>
2022-01-02 23:55:20 +0300

    update
    update

--- sumo_bot.ino
+++ sumo_bot.ino
@@ -1,17 +1,22 @@
+#include <QTRSensors.h>
+
 #define motor_r_e 3
-#define motor_r_in1 4
-#define motor_r_in2 5
+#define motor_r_in1 12
+#define motor_r_in2 10
 
 #define motor_l_e 6
-#define motor_l_in1 7
-#define motor_l_in2 8
+#define motor_l_in1 9
+#define motor_l_in2 11
 
 #define mz80 2
 
 int run_away_state = 0; // 0 - None; 1 - Go left; 2 - Go right
 int attack_state = 0; // 0 - find; 1 - Go forward;
 
-static const uint8_t line_sensors = {A0,A1,A2,A3,A4,A5,A6,A7}; // from left to right
+QTRSensors qtr;
+
+const uint8_t SensorCount = 8;
+uint16_t sensorValues[SensorCount];
 
 void setup() {
   pinMode(mz80, INPUT);
@@ -23,62 +28,84 @@ void setup() {
   pinMode(motor_l_in1, OUTPUT);
   pinMode(motor_l_in2, OUTPUT);
 
-  for (int i = 0; i < 8; i++) {
-      pinMode(line_sensors[i], INPUT);
-    }
+  Serial.begin(9600);
 
+  qtr.setTypeAnalog();
+  qtr.setSensorPins((const uint8_t[]){A7, A6, A5, A4, A3, A2, A1, A0}, SensorCount);
+  qtr.setEmitterPin(2);
+
+  delay(500);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  for (uint16_t i = 0; i < 400; i++)
+  {
+    qtr.calibrate();
+  }
+  digitalWrite(LED_BUILTIN, LOW);
+
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.minimum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+
+
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.maximum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  Serial.println();
+  delay(1000);
 }
 
-void get_direction() {
-    bool danger = false;
-    for (int i = 0; i < 4; i++) { // left
-      danger = (digitalRead(line_sensors[i]) == HIGH);
-      if (danger) { run_away_state = 2; return; }
-    }
-    
-     for (int i = 4; i < 8; i++) { // right
-      danger = (digitalRead(line_sensors[i]) == HIGH);
-      if (danger) { run_away_state = 1; return; }
-    }
-
-    run_away_state = 0;
-  }
-
-void move_robot(speedL, in1L, in2L, speedR, in1R, in2R) {
-  analogWrite(motor_l_e, speedL);
-  digitalWrite(motor_l_in1, in1L);
-  digitalWrite(motor_l_in2, in2L);
-
-  analogWrite(motor_r_e, speedR);
-  digitalWrite(motor_r_in1, in1R);
-  digitalWrite(motor_r_in2, in2R);
-  }
-
-void find_enemy() {
-    float distance = get_distance();
-    if (distance < thershhold) {
-      move_robot(255, HIGH, LOW, HIGH, LOW);
-      attack_state = 0;
-    }
-    else {
-      attack_state = 1;
-    }
-  
-  }
-
-float get_disance() {
-  }
+unsigned int sensor_values[8];
 
 void loop() {
-  get_direction();
-  if (run_away_state != 0) {
-      switch run_away_state:
-        case 1:
-          move_robot(200, LOW, LOW, HIGH, LOW);
-        case 2:
-          move_robot(200, HIGH, LOW, LOW, LOW);;
+  uint16_t position = qtr.readLineBlack(sensorValues);
+
+  // print the sensor values as numbers from 0 to 1000, where 0 means maximum
+  // reflectance and 1000 means minimum reflectance, followed by the line
+  // position
+  int sum_left = 0;
+  int sum_right = 0;
+  for (uint8_t i = 0; i < SensorCount/2; i++)
+  {
+    sum_left += sensorValues[i]/1000;
+    sum_right += sensorValues[i+4]/1000;
+  }
+  Serial.println(sum_left);
+  Serial.println(sum_right);
+
+  /*if (sum_left > 1) {
+      analogWrite(motor_r_e, 50);
+      digitalWrite(motor_r_in1, LOW);
+      digitalWrite(motor_r_in2, LOW);
+    
+      analogWrite(motor_l_e, 50);
+      digitalWrite(motor_l_in1, HIGH);
+      digitalWrite(motor_l_in2, LOW);    
+    }
+  else if (sum_right > 1) {
+      analogWrite(motor_r_e, 50);
+      digitalWrite(motor_r_in1, HIGH);
+      digitalWrite(motor_r_in2, LOW);
+    
+      analogWrite(motor_l_e, 50);
+      digitalWrite(motor_l_in1, LOW/);
+      digitalWrite(motor_l_in2, LOW);       
     }
   else {
-    move_robot(255, HIGH, LOW, HIGH, LOW);
-    } 
+      analogWrite(motor_r_e, 50);
+      digitalWrite(motor_r_in1, HIGH);
+      digitalWrite(motor_r_in2, LOW);
+    
+      analogWrite(motor_l_e, 50);
+      digitalWrite(motor_l_in1, HIGH);
+      digitalWrite(motor_l_in2, LOW);
+    }*/
+
 }
